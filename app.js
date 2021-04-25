0495a433559f461a1f04bcb6d965665e33fb8226
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-25 11:27:50 -0400

    Checks if request failed, redirects accordingly

--- app.js
+++ app.js
@@ -5,9 +5,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const https = require("https");
+const { response } = require("express");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 
 // ┌─────────────────────┐
@@ -22,6 +23,7 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 })
 
+// Post call, root
 app.post("/", function(req, res){
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
@@ -48,21 +50,37 @@ app.post("/", function(req, res){
     // Added us{x}
     const url = "region | endpoint | list id";
 
+    // options
     const options = {
         method: "POST",
-        auth: "username:API key here"
+        auth: "USERNAME:API KEY HERE"
     }
+
+    console.log(response.statusCode)
 
     // Make https request to store data
     const request = https.request(url, options, function(response){
+        
+        // Check if request was successful
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        }else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
         response.on("data", function(data){
-        console.log(JSON.parse(data));
+        //console.log(JSON.parse(data));
     })})
 
     // Write data to the server
     request.write(jsonData);
     request.end();
 });
+
+// Post call at failure.html
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
 
 app.listen(port, function(req, res){
     console.log("Server is running on port " + port);
